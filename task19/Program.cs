53f03884d1871ff19537150de7c93b247ee8cbe6
Evgenuu Sakin <shve-ster@yandex.ru>
2022-09-27 19:50:20 +0300

    lastshans seminar

---
+++ task19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void thatPalindrome(string number) 
+{
+    if (number[0] == number[4] && number[1] == number[3]) Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Число НЕ является палиндромом");
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+string num = Console.ReadLine();
+thatPalindrome(num);
---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число 
+//(N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int[] CubesSequence(int num) 
+{
+   int[] cubes = new int [num];
+   for (int i = 0; i < num; i++) {
+    cubes[i] = (i+1)*(i+1)*(i+1);
+   }
+   return cubes;
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int [] cubesList = CubesSequence(number);
+for (int i = 0; i < number; i++) Console.Write($"{cubesList[i]} ");
